257e7e0b40820fc8a405c3db243c5a3a9a365730
Phillip Maddox <philliptmaddox@gmail.com>
2013-06-04 22:51:07 -0500

    blowed it up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EcoTrends::Application.config.secret_token = 'fcdb88be3f7437b4eafda9b0839066ac86fb853c8c4433e73d337bb7f5507a77994d2779fc4ab062de13cb2b7a2f3a69f3e07dc37abbb250e3bfd69841fc25cd'
