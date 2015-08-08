a96b0ca3c22e8e22de5e2a7c9eb0e6e7cccec94c
Philip Ingram <philip@ingraminternet.com>
2015-08-08 06:18:32 -0400

    added pagination of 10 results, and moved to get based form tags

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,22 +1,20 @@
 class SearchController < ApplicationController
   before_filter :search_setup
   def index
-
+    @results = ExtracatMetadata.search(search_params).page(params[:page])
+    @search_term = params["keywords"]
   end
   def show
     @data = ExtracatMetadata.get_values(params[:id])
   end
-  def create
-    @results = ExtracatMetadata.search(search_params)
-    @search_term = params["keywords"]
-    respond_to do |format|
-      format.html { render :index }
-    end
-  end
+  # def create
+  #   respond_to do |format|
+  #     format.html { render :index }
+  #   end
+  # end
 
   def search_params
-    params.permit(:site_name, :id, :keywords)
-    #binding.pry
+    params.permit(:site_name, :id, :keywords, :page)
   end
   def search_setup
     @sites = ExtracatMetadata.sites
