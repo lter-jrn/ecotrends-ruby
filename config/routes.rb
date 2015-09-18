6aa3c9c71f868b8bb363e2d5859239fe50b8ff44
Philip Ingram <philip@ingraminternet.com>
2015-09-18 11:29:45 -0400

    updating routes to take a root url

--- config/routes.rb
+++ config/routes.rb
@@ -79,7 +79,7 @@ EcoTrends::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'splash#index'
+  root :to => 'splash#index'
 
   # See how all your routes lay out with "rake routes"
 
