d6f298d2f13e16285af80868b9361d45a961c7a5
Philip Ingram <philip@ingraminternet.com>
2015-09-25 11:50:49 -0400

    adding multiple checkbox dataset saves

--- app/controllers/datastores_controller.rb
+++ app/controllers/datastores_controller.rb
@@ -4,12 +4,16 @@ class DatastoresController < ApplicationController
   end
 
   def create
-    if params[:docid].present? && current_user.present?
-      s = SavedDataset.new
-      s.iduser = current_user_id
-      s.docid = params[:docid] #see if this works
-      is_not = s.save ? "" : "not "
+    is_not = "not "
+    if params[:docids].present? && current_user.present?
+      params[:docids].each do |doc|
+        s = SavedDataset.new
+        s.iduser = current_user_id
+        s.docid = doc #see if this works
+        is_not = "" if s.save
+      end
+
     end
-    redirect_to datastores_path, notice: "Dataset was #{is_not}saved"
+    redirect_to datastores_path, notice: "Dataset was {is_not}saved"
   end
 end
