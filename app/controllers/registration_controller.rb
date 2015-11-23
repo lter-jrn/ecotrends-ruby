95bc0e4fbdab995ad7ee25e0aaa2bb7ba50646d5
Philip Ingram <philip@ingraminternet.com>
2015-11-23 18:07:57 -0500

    updating for login after successful signup and msg if not successful.

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,11 +1,12 @@
 class RegistrationController < ApplicationController
-   #u= UserProfile.new(uid: "pjamm", regdate: DateTime.now, status:1, gname: "Philip", sname: "Test", net:"ECOTRENDS", org: "NSMU Test", email: "philip@ingram.com", phone: "212-555-1212")
+  #u= UserProfile.new(uid: "pjamm", regdate: DateTime.now, status:1, gname: "Philip", sname: "Test", net:"ECOTRENDS", org: "NSMU Test", email: "philip@ingram.com", phone: "212-555-1212")
   def new
     @user = UserProfile.new
   end
 
   def create
     the_route = "/signup"
+    msg = "You were not logged into your Network."
     if params["password"].present? && (params["password"] == params["password_confirmation"])
       network = "ECOTRENDS"
       if params["network"].present?
@@ -15,16 +16,21 @@ class RegistrationController < ApplicationController
         else
           network = ""
         end
-        Rails.logger.info(muck.inspect)
       end
       if network.present?
         @user = UserProfile.new(regdate: DateTime.now, uid: params["uid"], status: 1, gname: params["gname"], sname: params["sname"], net: network, org: params["org"], email: params["email"], phone: params["phone"], passphrase: UserProfile.encrypt_password(params["password"]))
-      else
-        the_route = "/"
-      end
-      the_route = "/search" if @user.save
-    end
-    redirect_to the_route
-  end
+        if @user.save
+          the_route = "/search"
+          msg = "Successfully signed up"
 
+          #session stuff here
+          session["current_user"] = @user.uid
+          session["iduser"] = @user.iduser
+        end
+
+      end
+      redirect_to the_route, notice: msg
+    end
+
+  end
 end
