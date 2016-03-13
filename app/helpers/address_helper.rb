b9cc19c6b8090929a50e627a1ba5ae5ae08a7d9c
Matthew Wallace <m@mattwallace.me>
2016-03-13 11:29:20 -0500

    cleaned up the metadata html page some more

--- app/helpers/address_helper.rb
+++ app/helpers/address_helper.rb
@@ -9,18 +9,20 @@ module AddressHelper
 
     html_string = ""
 
-    if delivery_point.kind_of?(String) == true
-      delivery_point = delivery_point.split(",")
-    end
-
-    delivery_point.each_with_index do |point, index|
-
-      if index < 1
-        html_string += "#{point}, "
-      else
-        html_string += "#{point}</br>"
+    if delivery_point.nil? == false
+      if delivery_point.kind_of?(String) == true
+        delivery_point = delivery_point.split(",")
       end
 
+      delivery_point.each_with_index do |point, index|
+
+        if index < 1
+          html_string += "#{point}, "
+        else
+          html_string += "#{point}</br>"
+        end
+
+      end
     end
 
     html_string += "#{city}, #{state} #{zip} #{contry}</br>"
